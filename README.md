c737cd423cc9cbab631da903f7b86972a23b0486
Vladislav Chernyshev <vlad-ski-96borsh@ya.ru>
2025-06-29 12:48:48 +0300

    after 1st review

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 https://github.com/vladislav25v/zakrivayuschiy-teg-f
+
 https://vladislav25v.github.io/zakrivayuschiy-teg-f/
--- scripts/like.js
+++ scripts/like.js
@@ -8,19 +8,12 @@
 Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
 */
 
-const likeHeartArray = document.querySelectorAll(
-  ".card__button-wrapper .like-icon"
-);
-const likeButtonArray = document.querySelectorAll(
-  ".card__button-wrapper .card__like-button"
-);
-const iconButtonArray = document.querySelectorAll(
-  ".card__button-wrapper .card__icon-button"
-);
+const likeHeartArray = document.querySelectorAll(".card__button-wrapper .like-icon");
+const likeButtonArray = document.querySelectorAll(".card__button-wrapper .card__like-button");
+const iconButtonArray = document.querySelectorAll(".card__button-wrapper .card__icon-button");
 
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
-    toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+  iconButton.onclick = () => toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
 });
 
 likeButtonArray.forEach((button, index) => {
@@ -34,59 +27,8 @@ function toggleIsLiked(heart, button) {
 
 function setButtonText(heart, button) {
   if ([...heart.classList].includes("is-liked")) {
-    setTimeout(
-      () => (button.querySelector(".button__text").textContent = "Unlike"),
-      500
-    );
+    setTimeout(() => (button.querySelector(".button__text").textContent = "Unlike"), 500);
   } else {
-    setTimeout(
-      () => (button.querySelector(".button__text").textContent = "Like"),
-      500
-    );
+    setTimeout(() => (button.querySelector(".button__text").textContent = "Like"), 500);
   }
 }
-
-document.querySelectorAll("[data-standalone-like]").forEach((wrapper) => {
-  const heartButtons = wrapper.querySelectorAll(".card__icon-button");
-  const image = wrapper.closest(".card").querySelector(".card__image");
-  const description = wrapper
-    .closest(".card")
-    .querySelector(".card__image-description");
-
-  heartButtons.forEach((btn) =>
-    btn.querySelector(".like-icon").classList.add("is-liked")
-  );
-
-  heartButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      heartButtons.forEach((btn, i) => {
-        const heart = btn.querySelector(".like-icon");
-        if (i >= index) {
-          heart.classList.remove("is-liked");
-        } else {
-          heart.classList.add("is-liked");
-        }
-      });
-
-      updateImageFilter();
-    });
-  });
-
-  function updateImageFilter() {
-    const activeCount = [...heartButtons].filter((btn) =>
-      btn.querySelector(".like-icon").classList.contains("is-liked")
-    ).length;
-
-    const filterSature = 25 * (7 - activeCount);
-    const filterContrast = 5 - activeCount;
-    image.style.filter = `saturate(${filterSature}%) contrast(${filterContrast})`;
-
-    if (description) {
-      if (activeCount === 0) {
-        description.classList.remove("visually-hidden");
-      } else {
-        description.classList.add("visually-hidden");
-      }
-    }
-  }
-});
